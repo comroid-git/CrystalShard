fed978d3dc74c1cd5c3a448191378a1bcd0d4ed6
burdoto-home <endermanialp@gmail.com>
2018-08-31 21:21:20 +0200

    Refined DiscordEventDispatch

--- src/main/java/de/kaleidox/crystalshard/internal/core/net/DiscordEventDispatch.java
+++ src/main/java/de/kaleidox/crystalshard/internal/core/net/DiscordEventDispatch.java
@@ -4,28 +4,32 @@ import com.fasterxml.jackson.databind.JsonNode;
 import de.kaleidox.crystalshard.internal.DiscordInternal;
 import de.kaleidox.crystalshard.internal.core.handlers.HandlerBase;
 import de.kaleidox.crystalshard.internal.core.net.socket.OpCode;
+import de.kaleidox.crystalshard.internal.core.net.socket.WebSocketClient;
 import de.kaleidox.logging.Logger;
-import de.kaleidox.util.helpers.JsonHelper;
-
-import java.lang.reflect.InvocationTargetException;
 
 public class DiscordEventDispatch {
     private final static Logger logger = new Logger(DiscordEventDispatch.class);
 
     public static void handle(DiscordInternal discord, JsonNode data) {
+        WebSocketClient webSocket = discord.getWebSocket();
         OpCode.getByCode(data.get("op").asInt())
                 .ifPresent(opCode -> {
-                    JsonNode node = data.get("d");
+                    logger.trace("Recieved Packet with OpCode "+opCode+" and body: "+data.toString());
                     switch (opCode) {
                         case HELLO:
-                            long heartbeat_interval = (node.get("heartbeat_interval").asLong());
+                            long heartbeat_interval = (data.get("d").get("heartbeat_interval").asLong());
                             discord.getThreadPool().startHeartbeat(heartbeat_interval);
                             break;
                         case DISPATCH:
                             discord.getThreadPool().execute(() -> dispatch(discord, data));
                             break;
                         case HEARTBEAT:
-                            discord.getWebSocket().heartbeat();
+                            webSocket.heartbeat();
+                            break;
+                        case HEARTBEAT_ACK:
+                            if (webSocket.respondToHeartbeat()) {
+                                webSocket.heartbeat();
+                            }
                             break;
                         default:
                             logger.warn("Unexpected OpCode recieved: " + opCode + " with body: " + data + "\n" +
@@ -36,17 +40,8 @@ public class DiscordEventDispatch {
     }
 
     private static <T extends HandlerBase> void dispatch(DiscordInternal discord, JsonNode data) {
-        try {
-            logger.trace("Dispatching event '"+data.get("t").asText()+"' with body: "+data.get("d").toString());
-            Package handlerPackage = HandlerBase.class.getPackage();
-            String t1 = handlerPackage.getName() + "." + data.get("t").asText();
-            Class<T> tClass = (Class<T>) Class.forName(t1);
-            HandlerBase t = tClass.getConstructor().newInstance();
-            discord.getThreadPool().execute(() -> t.handle(data.get("d")));
-        } catch (ClassNotFoundException e) {
-            logger.exception(e, "Failed to dispatch unknown type: " + data.get("t"));
-        } catch (NoSuchMethodException | IllegalAccessException | InstantiationException | InvocationTargetException e) {
-            logger.exception(e, "Failed to create instance of handler: "+data.get("t"));
-        }
+        logger.trace("Dispatching event '"+data.get("t").asText()+"' with body: "+data.get("d").toString());
+        T handler = HandlerBase.getHandlerByType(discord, data);
+        handler.handle(discord, data.get("d"));
     }
 }
