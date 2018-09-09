3c0caef468ee42d05b64b1a297b89bc399002b24
burdoto-home <endermanialp@gmail.com>
2018-09-09 17:19:30 +0200

    Added some simple annotations

---
+++ src/main/java/de/kaleidox/util/annotations/MayContainNull.java
@@ -0,0 +1,14 @@
+package de.kaleidox.util.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+import java.lang.reflect.Array;
+import java.util.Collection;
+
+/**
+ * This annotation marks a {@link Collection} or {@code Array} parameter to enable it to contain {@code null} items.
+ * @see Nullable
+ */
+@Target(ElementType.PARAMETER)
+public @interface MayContainNull {
+}
