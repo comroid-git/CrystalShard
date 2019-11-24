e9741613e68ad40db3d8e30ab05111d02da25ec9
Kaleidox <burdoto@outlook.com>
2019-11-24 02:08:59 +0100

    added combine method

--- api/src/main/java/de/comroid/crystalshard/util/model/NonThrowingCloseable.java
+++ api/src/main/java/de/comroid/crystalshard/util/model/NonThrowingCloseable.java
@@ -5,4 +5,10 @@ import java.io.Closeable;
 public interface NonThrowingCloseable extends Closeable {
     @Override
     void close();
+    
+    static NonThrowingCloseable combine(NonThrowingCloseable... nonThrowingCloseables) {
+        return () -> {
+            for (NonThrowingCloseable closeable : nonThrowingCloseables) closeable.close();
+        };
+    }
 }
