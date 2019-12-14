f7c8a9663575ade2299267b11e8e352908699b5d
Tobias Burdow [Kaleidox] <kaleidox@comroid.de>
2019-12-15 00:45:24 +0100

    remove nullable annotation

--- api/src/main/java/de/comroid/crystalshard/util/model/serialization/JsonDeserializable.java
+++ api/src/main/java/de/comroid/crystalshard/util/model/serialization/JsonDeserializable.java
@@ -7,14 +7,13 @@ import de.comroid.crystalshard.adapter.Constructor;
 import de.comroid.crystalshard.api.model.ApiBound;
 
 import com.alibaba.fastjson.JSONObject;
-import org.jetbrains.annotations.Nullable;
 
 @Constructor
 @SuppressWarnings("rawtypes")
 public interface JsonDeserializable extends ApiBound, Cloneable {
     Set<JSONBinding> bindings();
 
-    <S, T> @Nullable T getBindingValue(JSONBinding<?, S, ?, T> trait);
+    <S, T> T getBindingValue(JSONBinding<?, S, ?, T> trait);
 
     default <T> Optional<T> wrapBindingValue(JSONBinding<?, ?, ?, T> trait) {
         return Optional.ofNullable(getBindingValue(trait));
