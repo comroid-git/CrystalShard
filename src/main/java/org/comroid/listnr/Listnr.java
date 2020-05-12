a6e9988576cf4187937deb952cb8b3e937bd0d34
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2020-05-12 23:57:47 +0200

    whoa

--- src/main/java/org/comroid/listnr/Listnr.java
+++ src/main/java/org/comroid/listnr/Listnr.java
@@ -1,4 +1,68 @@
 package org.comroid.listnr;
 
+import org.comroid.common.Polyfill;
+import org.comroid.common.iter.pipe.Pipeable;
+import org.comroid.common.iter.pipe.Pump;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.function.Consumer;
+
 public @interface Listnr {
+    interface Attachable<IN, D, T extends EventType<IN, D, ? extends P>, P extends EventPayload<D, ? extends T>> {
+        ListnrCore<IN, D, T, P> getListnrCore();
+
+        default <ET extends EventType<IN, D, EP>, EP extends EventPayload<D, ET>> Listnr.API<IN, D, ET, EP> listenTo(ET eventType) {
+            if (!getListnrCore().getRegisteredEventTypes().contains(eventType))
+                throw new IllegalArgumentException(String.format("Type %s is not managed by %s", eventType, this));
+
+            return new Listnr.API<>(Polyfill.uncheckedCast(this), eventType);
+        }
+    }
+
+    final class API<IN, D, T extends EventType<IN, D, P>, P extends EventPayload<D, T>> implements Pipeable<P> {
+        private final Attachable<IN, D, T, P> attachable;
+        private final T eventType;
+
+        private API(Attachable<IN, D, T, P> attachable, T eventType) {
+            this.attachable = attachable;
+            this.eventType = eventType;
+        }
+
+        /**
+         * @param payloadConsumer The handler for the incoming payloads.
+         * @return A runnable that will detach the handler.
+         */
+        public final Runnable directly(Consumer<P> payloadConsumer) {
+            return attachable.getListnrCore().listen(attachable, eventType, payloadConsumer);
+        }
+
+        @Override
+        public final Pump<?, P> pipe() {
+            final Pump<P, P> pump = Pump.create();
+            final Runnable detacher = attachable.getListnrCore().listen(attachable, eventType, pump);
+            pump.addChildren(detacher::run);
+
+            return pump;
+        }
+
+        public final CompletableFuture<P> once() {
+            class FutureCompleter implements Consumer<P> {
+                private final CompletableFuture<P> future = new CompletableFuture<>();
+
+                @Override
+                public synchronized void accept(P p) {
+                    if (future.isDone())
+                        throw new IllegalStateException("Future is already completed");
+
+                    future.complete(p);
+                }
+            }
+
+            final FutureCompleter completer = new FutureCompleter();
+            final Runnable detacher = attachable.getListnrCore().listen(attachable, eventType, completer);
+            completer.future.thenRunAsync(detacher, Runnable::run);
+
+            return completer.future;
+        }
+    }
 }
--- src/main/java/org/comroid/listnr/ListnrCore.java
+++ src/main/java/org/comroid/listnr/ListnrCore.java
@@ -2,15 +2,27 @@ package org.comroid.listnr;
 
 import org.comroid.common.Polyfill;
 import org.comroid.common.info.Dependent;
+import org.comroid.common.iter.pipe.Pipe;
+import org.comroid.common.iter.pipe.Pipeable;
+import org.comroid.common.iter.pipe.Pump;
+import org.jetbrains.annotations.ApiStatus.Internal;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Consumer;
 
-public abstract class ListnrCore<IN, D, ID, T extends EventType<IN, D, ? extends EventPayload<D, ? extends T>>>
+public abstract class ListnrCore<IN, D, T extends EventType<IN, D, ? extends P>, P extends EventPayload<D, ? extends T>>
         implements Dependent<D> {
     private final Collection<EventType<IN, D, ? extends T>> types = new ArrayList<>();
+    private final Map<Listnr.Attachable<IN, D, ? extends T, ? extends P>, EventConsumers> consumers = new ConcurrentHashMap<>();
     private final D dependent;
+
+    public Collection<EventType<IN, D, ? extends T>> getRegisteredEventTypes() {
+        return types;
+    }
 
     @Override
     public @Nullable D getDependent() {
@@ -21,7 +33,32 @@ public abstract class ListnrCore<IN, D, ID, T extends EventType<IN, D, ? extends
         this.dependent = dependent;
     }
 
+    @Internal
+    <EP extends P> Runnable listen(final Listnr.Attachable<IN, D, ? extends T, ? extends P> listener,
+                                   final EventType<IN, D, ? extends EP> eventType,
+                                   final Consumer<EP> payloadConsumer) {
+        consumers(listener, eventType).add(payloadConsumer);
+
+        return () -> consumers(listener, eventType).remove(payloadConsumer);
+    }
+
+    @Internal
     void register(EventType<IN, D, ?> type) {
         types.add(Polyfill.uncheckedCast(type));
+    }
+
+    private Collection<Consumer<? extends P>> consumers(
+            Listnr.Attachable<IN, D, ? extends T, ? extends P> attachable,
+            EventType<IN, D, ? extends P> type) {
+        return consumers.computeIfAbsent(attachable, EventConsumers::new)
+                .computeIfAbsent(type, key -> new ArrayList<>());
+    }
+
+    private class EventConsumers extends ConcurrentHashMap<EventType<IN, D, ? extends P>, Collection<Consumer<? extends P>>> {
+        private final Listnr.Attachable<IN, D, ? extends T, ? extends P> owner;
+
+        public EventConsumers(Listnr.Attachable<IN, D, ? extends T, ? extends P> owner) {
+            this.owner = owner;
+        }
     }
 }
