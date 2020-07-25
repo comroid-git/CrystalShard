efcdceffd268ebd25606ee110147fd1dec003e2a
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2020-07-25 21:18:25 +0200

    more payloads

--- src/main/java/org/comroid/crystalshard/core/gateway/payload/guild/GatewayPresenceUpdatePayload.java
+++ src/main/java/org/comroid/crystalshard/core/gateway/payload/guild/GatewayPresenceUpdatePayload.java
@@ -1,4 +1,48 @@
 package org.comroid.crystalshard.core.gateway.payload.guild;
 
-public class GatewayPresenceUpdatePayload {
+import org.comroid.common.info.MessageSupplier;
+import org.comroid.crystalshard.DiscordBot;
+import org.comroid.crystalshard.core.gateway.event.GatewayPayloadWrapper;
+import org.comroid.crystalshard.core.gateway.payload.AbstractGatewayPayload;
+import org.comroid.crystalshard.entity.Snowflake;
+import org.comroid.crystalshard.entity.guild.Role;
+import org.comroid.crystalshard.entity.user.User;
+import org.comroid.crystalshard.model.user.UserActivity;
+import org.comroid.mutatio.span.Span;
+import org.comroid.uniform.ValueType;
+import org.comroid.uniform.node.UniObjectNode;
+import org.comroid.varbind.annotation.RootBind;
+import org.comroid.varbind.bind.GroupBind;
+import org.comroid.varbind.bind.VarBind;
+
+public final class GatewayPresenceUpdatePayload extends AbstractGatewayPayload {
+    @RootBind
+    public static final GroupBind<AbstractGatewayPayload, DiscordBot> Root
+            = BaseGroup.rootGroup("gateway-presence-update");
+    public static final VarBind<UniObjectNode, DiscordBot, User, User> user
+            = Root.createBind("user")
+            .extractAsObject()
+            .andProvide(
+                    Snowflake.Bind.ID,
+                    (id, bot) -> bot.getSnowflake(Snowflake.Type.USER, id).get(),
+                    User.Bind.Root)
+            .onceEach()
+            .setRequired()
+            .build();
+    public static final VarBind<Long, DiscordBot, Role, Span<Role>> roles
+            = Root.createBind("roles")
+            .extractAs(ValueType.LONG)
+            .andResolve((id, bot) -> bot.getSnowflake(Snowflake.Type.ROLE, id)
+                    .requireNonNull(MessageSupplier.format("Role with ID %d not found", id)))
+            .intoCollection(Span::new)
+            .setRequired()
+            .build();
+    public static final int game
+            = Root.createBind("game")
+            .extractAsObject()
+            .andConstruct(UserActivity.Root)
+
+    public GatewayPresenceUpdatePayload(GatewayPayloadWrapper gpw) {
+        super(gpw);
+    }
 }
--- src/main/java/org/comroid/crystalshard/core/gateway/payload/guild/GatewayWebhooksUpdatePayload.java
+++ src/main/java/org/comroid/crystalshard/core/gateway/payload/guild/GatewayWebhooksUpdatePayload.java
@@ -1,4 +1,39 @@
 package org.comroid.crystalshard.core.gateway.payload.guild;
 
-public class GatewayWebhooksUpdatePayload {
+import org.comroid.common.info.MessageSupplier;
+import org.comroid.crystalshard.DiscordBot;
+import org.comroid.crystalshard.core.gateway.event.GatewayPayloadWrapper;
+import org.comroid.crystalshard.core.gateway.payload.AbstractGatewayPayload;
+import org.comroid.crystalshard.entity.Snowflake;
+import org.comroid.crystalshard.entity.channel.Channel;
+import org.comroid.crystalshard.entity.guild.Guild;
+import org.comroid.uniform.ValueType;
+import org.comroid.varbind.annotation.RootBind;
+import org.comroid.varbind.bind.GroupBind;
+import org.comroid.varbind.bind.VarBind;
+
+public final class GatewayWebhooksUpdatePayload extends AbstractGatewayPayload {
+    @RootBind
+    public static final GroupBind<AbstractGatewayPayload, DiscordBot> Root
+            = BaseGroup.rootGroup("gateway-webhooks-update");
+    public static final VarBind<Long, DiscordBot, Guild, Guild> guild
+            = Root.createBind("guild_id")
+            .extractAs(ValueType.LONG)
+            .andResolve((id, bot) -> bot.getSnowflake(Snowflake.Type.GUILD, id)
+            .requireNonNull(MessageSupplier.format("Guild with ID %d not found", id)))
+            .onceEach()
+            .setRequired()
+            .build();
+    public static final VarBind<Long, DiscordBot, Channel, Channel> channel
+            = Root.createBind("channel_id")
+            .extractAs(ValueType.LONG)
+            .andResolve((id, bot) -> bot.getSnowflake(Snowflake.Type.CHANNEL, id)
+            .requireNonNull(MessageSupplier.format("Channel with ID %d not found",  id)))
+            .onceEach()
+            .setRequired()
+            .build();
+
+    public GatewayWebhooksUpdatePayload(GatewayPayloadWrapper gpw) {
+        super(gpw);
+    }
 }
