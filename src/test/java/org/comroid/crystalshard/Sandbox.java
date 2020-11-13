622d19eebf8cb720e378a3e8c08b8c9a89cbe2b7
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2020-11-13 17:38:43 +0100

    Update Sandbox.java

--- src/test/java/org/comroid/crystalshard/Sandbox.java
+++ src/test/java/org/comroid/crystalshard/Sandbox.java
@@ -1,6 +1,20 @@
 package org.comroid.crystalshard;
 
-public final class Sandbox {
+import org.comroid.common.io.FileHandle;
+
+public final class Sandbox extends DiscordBot {
+    public static final FileHandle DIR = new FileHandle("/srv/dcb/tester/", true);
+    public static final FileHandle LOGIN = DIR.createSubDir("login");
+
+    @Override
+    protected String getToken() {
+        return LOGIN.createSubFile("discord.cred").getContent(true);
+    }
+
+    protected Sandbox(DiscordAPI context) {
+        super(context);
+    }
+
     public static void main(String[] args) {
     }
 }
