4bafe2e81d883a07716154a4a448d7286fdce5fd
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2020-11-14 17:17:07 +0100

    Added transitive versions

--- src/main/java/org/comroid/crystalshard/CrystalShard.java
+++ src/main/java/org/comroid/crystalshard/CrystalShard.java
@@ -3,9 +3,25 @@ package org.comroid.crystalshard;
 import org.comroid.api.Polyfill;
 import org.comroid.common.Version;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
+import java.util.Properties;
 
 public final class CrystalShard {
     public static final URL URL = Polyfill.url("https://github.com/comroid-git/CrystalShard");
-    public static final Version VERSION = new Version(0,42,0, Version.ReleaseType.SNAPSHOT,0);
+    public static final Version VERSION;
+
+    static {
+        try (
+                InputStream is = ClassLoader.getSystemResource("crystalshard.properties").openStream()
+        ) {
+            Properties prop = new Properties();
+            prop.load(is);
+
+            VERSION = new Version(prop.getProperty("version"));
+        } catch (IOException e) {
+            throw new RuntimeException("Could not load CrystalShard", e);
+        }
+    }
 }
