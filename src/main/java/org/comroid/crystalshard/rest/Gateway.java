b4aa749108c594feaac7e2d4affdf422224db93d
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2022-12-24 12:46:49 +0100

    Update Gateway.java

--- src/main/java/org/comroid/crystalshard/rest/Gateway.java
+++ src/main/java/org/comroid/crystalshard/rest/Gateway.java
@@ -4,9 +4,11 @@ import org.comroid.crystalshard.rest.response.GatewayBotResponse;
 import org.comroid.spring.util.model.ReverseController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @ReverseController("/gateway")
 public interface Gateway {
-    @GetMapping("/bot")
+    @RequestMapping(value = "/bot", method = RequestMethod.GET)
     GatewayBotResponse getBot(@RequestHeader("Authorization") String token);
 }
